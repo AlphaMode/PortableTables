70138ab96a85ded80442d13bb7bc2b6b89f8da81
alpha <alphamodeyt@gmail.com>
2022-07-24 15:47:23 -0500

    Add textures

--- src/main/java/me/alphamode/portablecrafting/mixin/InventoryScreenMixin.java
+++ src/main/java/me/alphamode/portablecrafting/mixin/InventoryScreenMixin.java
@@ -1,6 +1,7 @@
 package me.alphamode.portablecrafting.mixin;
 
 import me.alphamode.portablecrafting.client.PortableWidget;
+import me.alphamode.portablecrafting.tables.AllTables;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
@@ -30,9 +31,9 @@ public abstract class InventoryScreenMixin extends HandledScreen<PlayerScreenHan
         for (ClickableWidget widget : Screens.getButtons(this)) {
             if(widget instanceof PortableWidget craftingWidget) {
                 if (recipeBook.isOpen()) {
-                    craftingWidget.setPos(this.width / 2 + 120, this.height / 2 - 20);
+                    craftingWidget.setPos(this.width / 2 + (craftingWidget.getTableType() == AllTables.CRAFTING ? 120 : 140), this.height / 2 - 20);
                 } else {
-                    craftingWidget.setPos(this.width / 2 + 40, this.height / 2 - 20);
+                    craftingWidget.setPos(this.width / 2 + (craftingWidget.getTableType() == AllTables.CRAFTING ? 40 : 60), this.height / 2 - 20);
                 }
             }
         }
