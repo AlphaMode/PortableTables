55f23339b1315dba30f75d72878677c13c22e77d
alpha <alphamodeyt@gmail.com>
2023-07-10 12:38:43 -0500

    Use forge registries for ModelsGen forge

--- Forge/src/main/java/me/alphamode/portablecrafting/forge/data/ModelsGen.java
+++ Forge/src/main/java/me/alphamode/portablecrafting/forge/data/ModelsGen.java
@@ -2,9 +2,9 @@ package me.alphamode.portablecrafting.forge.data;
 
 import me.alphamode.portablecrafting.PortableTables;
 import net.minecraft.data.DataOutput;
-import net.minecraft.registry.Registries;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModelsGen extends ItemModelProvider {
     public ModelsGen(DataOutput output, ExistingFileHelper existingFileHelper) {
@@ -13,8 +13,8 @@ public class ModelsGen extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        Registries.ITEM.forEach(item -> {
-            if (Registries.ITEM.getId(item).getNamespace().equals("portable_tables"))
+        ForgeRegistries.ITEMS.forEach(item -> {
+            if (ForgeRegistries.ITEMS.getKey(item).getNamespace().equals("portable_tables"))
                 basicItem(item);
         });
     }
